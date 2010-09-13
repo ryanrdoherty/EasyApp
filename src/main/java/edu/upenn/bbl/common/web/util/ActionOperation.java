7fde4e9f6cca70adb7e6ef4f85e110a122d3f3cd
Ryan Doherty <ryanrobdoh@speakeasy.net>
2010-09-13 17:57:54 -0400

    minor updates

---
+++ src/main/java/edu/upenn/bbl/common/web/util/ActionOperation.java
@@ -0,0 +1,12 @@
+package edu.upenn.bbl.common.web.util;
+
+/**
+ * Represents an operation that can be performed on a record.
+ * 
+ * @author rdoherty
+ */
+public enum ActionOperation {
+	CREATE,
+	UPDATE,
+	DELETE;
+}
